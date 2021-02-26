fda011e37ebe6846fd5ea98a8a0d0b58100b0860
Andrew Park <apark999@gmail.com>
2021-02-25 22:48:34 -0800

    installed necessary packages, server js and schema added

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+const mysql = require("mysql");
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.use(require('./controllers/burgers_controller.js'));
+
+app.listen(PORT, () => {
+  console.log(`Server listening on: http://localhost: ${PORT}`);
+});
