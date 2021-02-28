7d8b0429c85d9d3a20118bc54bc945ec51a7fe23
Andrew Park <apark999@gmail.com>
2021-02-27 16:47:38 -0800

    added js files and burger png

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Rad Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mondo Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Awesome Burger', true);
